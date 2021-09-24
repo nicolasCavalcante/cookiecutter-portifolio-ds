4daf16849d912bc098e52a18e2e2710131d8acf9
nicolasCavalcante <nicolasgcavalcante@gmail.com>
2021-09-24 00:01:32 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### To start a new project, run:
+------------
+
+    cookiecutter https://github.com/nicolasCavalcante/cookiecutter-portifolio-ds.git
+------------
+
+    python -m cookiecutter https://github.com/nicolasCavalcante/cookiecutter-portifolio-ds.git
---
+++ {{ cookiecutter.repo_name}}/dodo.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import subprocess
+
+SELF_PATH = Path(__file__).parent.absolute()
+# DOIT_CONFIG = {'default_tasks': ['devinstall', 'autopep']}
+
+
+def syscmd(string):
+    subprocess.check_call(string, shell=True)
+    return True
+
+
+def task_devinstall():
+    """install development packages"""
+    for req in 'dev', 'prod':
+        yield {
+            'name': f'install {req} requeirments',
+            'actions': ['pip install -r requirements/%s.txt' % req],
+            'verbosity': 2,
+            'file_dep': [Path('requirements/' + req + '.txt')]
+        }
+
+
+def task_autopep():
+    """lint codebase using yapf acording to PEP8"""
+    def autopep(filepath: Path):
+        from yapf.yapflib.yapf_api import FormatFile
+        FormatFile(str(filepath), in_place=True)
+
+    nparts = len(SELF_PATH.parts)
+    for filepath in SELF_PATH.glob('**/*.py'):
+        yield {
+            'name': '/'.join(filepath.parts[nparts:]),
+            'actions': [(autopep, [], {
+                'filepath': filepath
+            })],
+            'file_dep': [filepath]
+        }
+
+
+def task_pytests():
+    """run pytests under tests folder"""
+
+    path = SELF_PATH / 'tests'
+    nparts = len(path.parts)
+    for filepath in (x for x in SELF_PATH.glob('**/*.py') if 'test' in x.name):
+        if not filepath.is_file():
+            continue
+        yield {
+            'name': '/'.join(filepath.parts[nparts:]),
+            'actions': [lambda: syscmd(f'pytest --testmon "{filepath}"')],
+            'verbosity': 2
+        }
