2e197fb42bfd21f187d3655cf2840a1a5abbafe8
nicolasCavalcante <nicolasgcavalcante@gmail.com>
2021-10-02 21:43:08 -0300

    feat: add mlflow command to dodo

--- {{ cookiecutter.repo_name}}/dodo.py
+++ {{ cookiecutter.repo_name}}/dodo.py
@@ -2,11 +2,11 @@ import platform
 import subprocess
 from pathlib import Path
 
-CMD_SEP = '&' if platform.system() == 'Windows' else ';'
+from {{cookiecutter.repo_name}}.utils import MLFLOW_DIR
 
+CMD_SEP = '&' if platform.system() == 'Windows' else ';'
 SELF_PATH = Path(__file__).parent.absolute()
-spathsrt = '"%s/"' % SELF_PATH.as_posix()
-# DOIT_CONFIG = {'default_tasks': ['format', 'pytest']}
+DOIT_CONFIG = {'default_tasks': ['format', 'pytest']}
 
 
 def syscmd(string):
@@ -15,17 +15,36 @@ def syscmd(string):
 
 
 def task_format():
-    return {
-        'actions': [
-            'autoflake -i -r --expand-star-imports --remove-all-unused-imports'
-            + ' --remove-duplicate-keys --remove-unused-variables .' +
-            ' %s isort . %s yapf -i -r .' % (CMD_SEP, CMD_SEP)
-        ],
-        'verbosity':
-        2
-    }
+    """makes code organized and pretty"""
+    nparts = len(SELF_PATH.parts)
+    for filepath in SELF_PATH.glob('**/*.py'):
+        if str(filepath).startswith(str(SELF_PATH / 'output')):
+            continue
+        yield {
+            'name':
+            '/'.join(filepath.parts[nparts:]),
+            'actions':
+            [('autoflake -i -r --expand-star-imports' +
+              ' --remove-all-unused-imports' +
+              ' --remove-duplicate-keys --remove-unused-variables %s' +
+              ' %s isort %s %s yapf -i -r %s') %
+             (filepath, CMD_SEP, filepath, CMD_SEP, filepath)],
+            'file_dep': [filepath],
+            'verbosity':
+            2
+        }
 
 
 def task_pytest():
     """run pytests under tests folder"""
     return {'actions': [lambda: syscmd('pytest tests/')], 'verbosity': 2}
+
+
+def task_ui():
+    """start mlflow ui"""
+    return {
+        'actions':
+        ['mlflow ui --backend-store-uri file:%s' % MLFLOW_DIR.as_posix()],
+        'verbosity':
+        2
+    }
